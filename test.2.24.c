d3fb3539068d8608a7dd5368ac0a2de195773592
guankunkun <2581116279@qq.com>
2021-02-24 23:55:23 +0800

    2.24

---
+++ test.2.24.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//交换数组
+//int main()
+//{
+//	int arr1[5] = { 0 };
+//	int arr2[5] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//
+//
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%d", &arr2[i]);
+//
+//
+//
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		int tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//
+//	}
+//	printf("\n");
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//
+//	}
+//	printf("\n");
+//
+//
+//	return 0;
+//}
+//三子棋
