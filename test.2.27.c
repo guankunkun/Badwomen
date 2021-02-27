9276946d573cb64f47725f14b3251cfc4703b540
guankunkun <2581116279@qq.com>
2021-02-27 22:35:54 +0800

    2.27

---
+++ test.2.27.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+
+	//int a=0, b=0, c=0;
+
+	//a = 5;
+	//c = ++a;
+	//b = ++c, c++, ++a, a++;
+	//b += a++ + c;
+	//printf("a = %d b = %d c = %d\n:", a, b, c);
+	//int guan = 10;
+	//int wang = 15;
+	//printf("½»»»Ç°guan=%d  wang=%d\n", guan, wang);
+	//guan = guan ^ wang;
+	//wang = guan ^ wang;
+	//guan = guan ^ wang;
+	//printf("%d %d", guan, wang);
+	//int i = 1;
+	//int ret = (++i) + (++i) + (++i);
+	//printf("ret = %d\n", ret);
+	//int i = 0;
+	//i--;
+	//if (i > sizeof(i))
+	//{
+	//	printf(">\n");
+	//}
+	//else
+	//{
+	//	printf("<\n");
+	//}
+
+
+	//	int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a, &b);
+
+	//int c = a ^ b;
+	//
+	//int count = 0;
+	//while (c) {
+	//	count++;
+	//	c = c & (c - 1);
+	//}
+
+	//printf("%d\n", count);
+
+
+
+
+
+
+	return 0;
+}
