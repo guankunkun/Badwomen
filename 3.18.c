86edde44942b5e04da532c395f95fd68b95343be
guankunkun <2581116279@qq.com>
2021-03-18 23:00:20 +0800

    3.18

---
+++ 3.18.c
@@ -0,0 +1,59 @@
+ #include<stdio.h>
+#include<string.h>
+//逆序字符串函数
+//void myreverse(char* left, char* right)
+//{
+//	while(left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = *left;
+//		left++;
+//		right--;
+//	}
+//	                                         
+//
+//}
+//int main()
+//{
+//	//最小公倍数
+//	/*int a = 0;
+//	int b = 0;
+//	int i = 0;
+//	while (a * i % b != 0)
+//	{
+//		i++;
+//	}
+//	printf("%d", a * i);*/
+//	//倒置字符串 l like beijing. - beijig.  like l
+//	char arr[100] = { 0 };
+//	//接收
+//	gets(arr);
+//	//scanf在乎空格，只接受空格之前的 ges芜锁胃
+//	int len = strlen(arr);
+//	myreverse(arr, arr + len - 1);
+//	//1.逆序整个字符串
+//	//2.逆序每一个单词
+//    //输出
+//	char* start = arr;
+//	char* end = arr;
+//	while (*start)
+//	{
+//		end = start;
+//		while (*end != ' '&& *end != '\0')
+//		{
+//			end++;
+//		}
+//		//end记录的是一个单词空格的地址
+//		myreverse(start, end - 1);
+//		//end-1就是单词最后一个字母的位置
+//		if (*end == '\0')
+//			start = end + 1;
+//		else
+//			start = end;
+//	}
+//	puts(arr);
+//
+
+	return 0;
+}
